0e6c12a21fa75ca197900c049d40722383cd3e25
Gabriel Perez <gperezpereira@danielcastelao.org>
2023-01-16 11:01:38 +0100

    actualizacion boletines

---
+++ SegundoTrimestre/Boletin_19/src/boletin_19/Boletin_19.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin_19;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin_19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Persoa person1=new Persoa();
+        person1.cantar();
+        
+        Canario canario1=new Canario();
+        canario1.cantar();
+    }
+    
+}
---
+++ SegundoTrimestre/Boletin_19/src/boletin_19/Persoa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boletin_19;
+
+/**
+ *
+ * @author dam1
+ */
+public class Persoa implements IPodeCantar{
+
+    @Override
+    public void cantar() {
+        System.out.println("DO RE MI FA SOL LA SI");
+    }
+    
+}
