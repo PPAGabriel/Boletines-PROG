fb3a0e7522f3d8d93cad2803a0712f7aee5ed342
Gabriel Perez <gperezpereira@danielcastelao.org>
2023-01-25 12:33:30 +0100

    actualizacion ejemplos

---
+++ Exemplos/ExemploMatriz/src/exemplomatriz/ExemploMatriz.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplomatriz;
+
+/**
+ *
+ * @author dam1
+ */
+public class ExemploMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MetodosMatrices obx1= new MetodosMatrices();
+        
+        int matriz1[][];
+        float array1[];
+        
+        matriz1=obx1.crearMatriz();
+        obx1.verMatriz(matriz1);
+        array1=obx1.notaMediaModulo(matriz1);
+        obx1.verNotaMedia(array1);
+    }
+    
+}
---
+++ Exemplos/ExemploMatriz/src/exemplomatriz/MetodosMatrices.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplomatriz;
+
+import com.gabriel.datos.PedirDatos;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dam1
+ */
+public class MetodosMatrices {
+    
+    int f,c;
+    
+    Random num=new Random();
+    
+    public int[][] crearMatriz(){
+        f=PedirDatos.PedirInt("Introduzca el nro. de filas:");
+        c=PedirDatos.PedirInt("Introduzca el nro. de columnas:");
+        
+    int [][] matriz=new int[f][c];
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
+          matriz[i][j]=PedirDatos.PedirInt("Introduzca un valor para la posición f:"+(i+1)+" c:"+(j+1));
+          
+          //con numeros aleatorios
+          //matriz[i][j]=num.nextInt(11);
+        }
+    }
+    return matriz;
+    }
+    
+    public void verMatriz(int [][]matriz){
+        for (int i=0;i<matriz.length;i++){
+            System.out.print("Alumno nro."+(i+1)+": ");
+            for (int j=0;j<matriz[i].length;j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public float[] notaMediaAlumno(int [][]matriz){
+        float nm=0;
+        float array[]=new float[matriz.length];
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+               nm+=matriz[i][j];
+          
+          //con numeros aleatorios
+          //matriz[i][j]=num.nextInt(11);
+                }
+            
+            array[i]=(float) (nm/matriz[i].length);
+            nm=0;
+            }
+        return array;
+        }
+    
+    public float[] notaMediaModulo(int [][]matriz){
+        float nm=0;
+        float array[]=new float[c];
+        for(int j=0;j<c;j++){
+            for(int i=0;i<f;i++){
+               nm+=matriz[i][j];
+          
+          //con numeros aleatorios
+          //matriz[i][j]=num.nextInt(11);
+                }
+            
+            array[j]=(float) (nm/f);
+            nm=0;
+            }
+        return array;
+        }
+    
+    public void verNotaMedia(float array[]){
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+   
+    
+}
