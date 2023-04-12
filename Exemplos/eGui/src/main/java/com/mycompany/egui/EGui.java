e5f0b6e7c4a49b55a8f7c44cc11452e897c15450
Gabriel Perez <gperezpereira@danielcastelao.org>
2023-04-12 10:43:00 +0200

    Actualizacion

---
+++ Exemplos/eGui/src/main/java/com/mycompany/egui/EGui.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.egui;
+
+import gui.Layouts;
+
+/**
+ *
+ * @author dam1
+ */
+public class EGui {
+
+    public static void main(String[] args) {
+                /*
+        Ventana1 v1 = new Ventana1();
+        v1.iniciarVentana();
+        */
+        /*
+        Ventana2 v2 = new Ventana2();
+        v2.iniciarVentana();
+        */
+        /*
+        Compoñentes v3 = new Compoñentes();
+        v3.iniciar();
+        */
+//      Layouts v4 = new Layouts();
+//      v4.flowLayout2();
+        Layouts v5 = new Layouts();
+        v5.disBoxLayout();
+    }
+ 
+}
+
---
+++ Exemplos/eGui/src/main/java/gui/Ventana2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+import java.awt.Color;
+import javax.swing.*;
+
+/**
+ *
+ * @author dam1
+ */
+public class Ventana2 extends JFrame{
+        //damoslle as características
+    
+    public void iniciarVentana(){
+        this.setSize(800, 400);
+        
+        /*
+        //Para usar un panel, podemos utilizar el default, para usar el color de fondo necesitamos un panel
+        JPanel panel = (JPanel) this.getContentPane(); 
+        panel.setBackground(Color.BLUE);
+        */
+        
+        
+        this.setDefaultCloseOperation(3);
+        this.setVisible(true);
+    }
+}
