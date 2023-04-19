d2b5f029ed73dd1093909410aeb1cef43f705126
Nuevo Nombre <gperezpereira@danielcastelao.org>
2023-04-19 12:03:36 +0200

    Boletin 31

---
+++ TercerTrimestre/Boletin_31/src/main/java/com/mycompany/boletin31/Boletin31.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin31;
+
+/**
+ *
+ * @author gabri
+ */
+public class Boletin31 {
+
+    public static void main(String[] args) {
+      Calculadora calc= new Calculadora();
+      calc.setVisible(true);
+    }
+}
