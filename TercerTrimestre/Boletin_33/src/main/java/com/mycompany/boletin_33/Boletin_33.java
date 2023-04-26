b0dbe143749cd6078a70dbee6147659026a34377
Gabriel Perez <gperezpereira@danielcastelao.org>
2023-04-26 10:31:52 +0200

    actualizacion 26-04

---
+++ TercerTrimestre/Boletin_33/src/main/java/com/mycompany/boletin_33/Boletin_33.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin_33;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin_33 {
+
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ TercerTrimestre/Boletin_33/src/main/java/com/mycompany/boletin_33/SeleccionFutbol.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.boletin_33;
+
+/**
+ *
+ * @author dam1
+ */
+public abstract class SeleccionFutbol {
+    protected Integer id;
+    protected String nombre;
+    protected String apellidos;
+    protected Integer edad;
+}
